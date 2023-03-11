bcf412a4d623114af4496cad415c9446c17f0042
Jay <78855012+ejionaut@users.noreply.github.com>
2023-03-11 11:13:09 +0800

    Update simulation.java

--- simulation.java
+++ simulation.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 
-public class FilesTest {
+public class simulation {
    static Scanner src = new Scanner(System.in);
    static Random rand = new Random();
    public static void main(String[] args) {
